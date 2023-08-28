136b387d34cd190343f78f9ef876c81a29013314
EetuPe <eetu.petanen@gmail.com>
2023-08-28 23:45:46 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,213 @@
-import Image from 'next/image'
+import Image from "next/image";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+//Icons
+import { DiReact, DiJsBadge, DiHtml5, DiCss3 } from "react-icons/di";
+import profile from "../public/profile.jpeg";
+import mentis from "../public/Mentis.png";
+import produni from "../public/Produni.png";
+import multitool from "../public/dmx_multitool.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <main className="bg-white px-10 min-h-screen md:px-20">
+        <section className="mb-20">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-adlam">EetuPe</h1>
+            <ul className="flex items-center">
+              <li>
+                <a
+                  className="bg-gradient-to-r from-red-500 to-rose-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="/CV.pdf"
+                  download="cv_eetu_petanen.pdf"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-left pt-10 px-10 flex justify-between">
+            <div>
+              <h2 className="text-5xl py-2 text-red-600 font-medium md:text-6xl">
+                Eetu Petänen
+              </h2>
+              <h3 className="text-2xl pt-2 md:text-3xl">
+                Developer and student.
+              </h3>
+              <div className="text-5xl flex justify-start py-4 gap-6 text-gray-600">
+                <a href="https://twitter.com/epetanen">
+                  <AiFillTwitterCircle />
+                </a>
+                <a href="https://www.linkedin.com/in/eetu-petanen/">
+                  <AiFillLinkedin />
+                </a>
+                <a href="https://www.github.com/EetuPe/">
+                  <AiFillGithub />
+                </a>
+              </div>
+            </div>
+            <div className="relative rounded-full w-40 h-40 overflow-hidden md:h-80 md:w-80">
+              <Image src={profile} layout="fill" objectFit="cover" />
+            </div>
+          </div>
+        </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <section className="px-10">
+          <h3 className="text-3xl py-1 dark:text-white">
+            Projects I've worked on:
+          </h3>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <div className="relative w-140 h-80 overflow-hidden md:h-60 md:w-140 rounded">
+                <Image src={produni} layout="fill" objectFit="cover" />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2  ">Produni</h3>
+              <p className="py-2">
+                A place for students to keep track of their schedules.
+              </p>
+              <div class="justify-center my-8 select-none flex">
+                <a
+                  href="https://github.com/EetuPe/produni1.1"
+                  className="py-2 px-4 shadow-md no-underline rounded-full bg-blue-500 text-white font-sans font-semibold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2"
+                >
+                  {`</>`}
+                </a>
+                <a
+                  href="https://produni.petanen.com"
+                  className="py-2 px-4 shadow-md no-underline rounded-full bg-orange-500 text-white font-sans font-semibold text-sm border-orange btn-primary hover:text-white hover:bg-orange-light focus:outline-none active:shadow-none mr-2"
+                >
+                  Live View
+                </a>
+              </div>
+              <div className="justify-center my-8 select-none flex">
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiReact />
+                  <span class="block text-sm font-medium">React</span>
+                </div>
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiJsBadge />
+                  <span class="block text-sm font-medium">Javascript</span>
+                </div>
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiHtml5 />
+                  <span class="block text-sm font-medium">HTML</span>
+                </div>
+              </div>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <div className="relative w-140 h-80 overflow-hidden md:h-60 md:w-140 rounded">
+                <Image src={mentis} layout="fill" objectFit="cover" />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Mentis</h3>
+              <p className="py-2">
+                A mastermind game where the player can guess the 4 color code by
+                inputting a 4-digit code. Each color is assigned a numerical
+                value.
+              </p>
+              <div class="justify-center my-8 select-none flex">
+                <a
+                  href="https://github.com/EetuPe/mentis"
+                  className="py-2 px-4 shadow-md no-underline rounded-full bg-blue-500 text-white font-sans font-semibold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2"
+                >
+                  {`</>`}
+                </a>
+                <a
+                  href="https://mentis.petanen.com"
+                  className="py-2 px-4 shadow-md no-underline rounded-full bg-orange-500 text-white font-sans font-semibold text-sm border-orange btn-primary hover:text-white hover:bg-orange-light focus:outline-none active:shadow-none mr-2"
+                >
+                  Live View
+                </a>
+              </div>
+              <div className="justify-center my-8 select-none flex">
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiReact />
+                  <span class="block text-sm font-medium">React</span>
+                </div>
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiJsBadge />
+                  <span class="block text-sm font-medium">Javascript</span>
+                </div>
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiHtml5 />
+                  <span class="block text-sm font-medium">HTML</span>
+                </div>
+              </div>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <div className="relative w-140 h-80 overflow-hidden md:h-60 md:w-140 rounded">
+                <Image src={multitool} layout="fill" objectFit="cover" />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">DMX Multitool</h3>
+              <p className="py-2">
+                A tool for lighting technicians to calculate DMX channel values
+                for all kinds of fixtures. Works with multiple universes.
+              </p>
+              <div class="justify-center my-8 select-none flex">
+                <a
+                  href="https://dmxtool.com"
+                  className="py-2 px-4 shadow-md no-underline rounded-full bg-orange-500 text-white font-sans font-semibold text-sm border-orange btn-primary hover:text-white hover:bg-orange-light focus:outline-none active:shadow-none mr-2"
+                >
+                  Live View
+                </a>
+              </div>
+              <div className="justify-center my-8 select-none flex">
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiReact />
+                  <span class="block text-sm font-medium">React</span>
+                </div>
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiJsBadge />
+                  <span class="block text-sm font-medium">Javascript</span>
+                </div>
+                <div
+                  role="button"
+                  title="Hover chip"
+                  class="h-8 px-3 w-max flex gap-2 items-center rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 hover:bg-opacity-75 focus:bg-gray-300 focus:text-blue-900 active:text-primary active:bg-blue-100 disabled:bg-gray-100 disabled:text-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:active:text-primary"
+                >
+                  <DiHtml5 />
+                  <span class="block text-sm font-medium">HTML</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
